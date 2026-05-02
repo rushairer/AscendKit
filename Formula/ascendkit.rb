0c8a60c47ada0f649201b395eb426ea8df2cb119
Aben <me@aben.io>
2026-05-02 18:09:08 +0800

    Sync Homebrew formula for v0.32.0

--- Formula/ascendkit.rb
+++ Formula/ascendkit.rb
@@ -2,7 +2,7 @@ class Ascendkit < Formula
   desc "Local-first App Store release preparation toolkit"
   homepage "https://github.com/rushairer/AscendKit"
   url "https://github.com/rushairer/AscendKit/releases/download/v0.32.0/ascendkit-0.32.0-macos-arm64.tar.gz"
-  sha256 "24d0623e1756abc34ce72f3e31318ebbdcbb8403928572edb777f7ca0221a5b4"
+  sha256 "20cf1108b90c4084b5f61fd44abc0b7278f49cf7a6c6ef995165436804639f3d"
   license "MIT"
 
   def install
