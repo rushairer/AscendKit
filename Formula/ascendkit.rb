87d2d784e5cf88a3b97942538daac6f8b641b6ee
Aben <me@aben.io>
2026-05-08 11:02:15 +0800

    Sync formula checksum for v1.4.3

--- Formula/ascendkit.rb
+++ Formula/ascendkit.rb
@@ -2,7 +2,7 @@ class Ascendkit < Formula
   desc "Local-first App Store release preparation toolkit"
   homepage "https://github.com/rushairer/AscendKit"
   url "https://github.com/rushairer/AscendKit/releases/download/v1.4.3/ascendkit-1.4.3-macos-universal.tar.gz"
-  sha256 "5820f58c2af0b3767f8444be9ad3a63ac93355429ba75f5f51d99b219f004e09"
+  sha256 "476551215dc34b9742b96265d5ecad0905c61fce82e6e6198b08214893d3000e"
   license "MIT"
 
   def install
