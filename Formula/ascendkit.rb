8f53531b1d96349e6635957cd7e1c4560c3f4cce
Paw <paw@aben.io>
2026-06-03 22:35:29 +0800

    Fix formula checksum to match published v1.8.0 release asset

--- Formula/ascendkit.rb
+++ Formula/ascendkit.rb
@@ -2,7 +2,7 @@ class Ascendkit < Formula
   desc "Local-first App Store release preparation toolkit"
   homepage "https://github.com/rushairer/AscendKit"
   url "https://github.com/rushairer/AscendKit/releases/download/v1.8.0/ascendkit-1.8.0-macos-universal.tar.gz"
-  sha256 "7efc63239c92d7927a358295888eeb42164d6ba179a9745c55e6d1dee7bf9e45"
+  sha256 "a4587b20d928cd62d27a1233823baf3fc698fd87f92d1a587959089ccedb7f6b"
   license "MIT"
 
   def install
